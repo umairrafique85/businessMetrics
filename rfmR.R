1f6a1d067bb90def49cd4bdb8a24bfaea03009b2
Umair Rafique <umairrafique@marketlytics.com>
2018-12-14 19:50:31 +0500

    Two functions made. function to output scores remaining

--- rfmR.R
+++ rfmR.R
@@ -1,40 +1,39 @@
 
 ## TODO: give choice to calculate monetary value by summing or by averaging  
-
-
 library(Hmisc)
 
-# deal with negative values in quantity and unitPrice
-df_retailData$Quantity[which(is.na(df_retailData$Quantity))] <- NA
-df_retailData$UnitPrice[which(is.na(df_retailData$UnitPrice))] <- NA
+# function to get metrics, monetary by sum
+getRFM_metrics <- function(customerId, invoiceDate, dateBaseline = Sys.Date(), 
+                           invoiceNumber, invoiceAmount, dataframe){
+  customerId <- enquo(customerId)
+  invoiceDate <- enquo(invoiceDate)
+  dateBaseline <- enquo(dateBaseline)
+  invoiceNumber <- enquo(invoiceNumber)
+  invoiceAmount <- enquo(invoiceAmount)
+  as.data.frame(dataframe) %>% group_by(customerId = !!customerId) %>% 
+    summarise(recency = as.integer(!!dateBaseline - max(!!invoiceDate)),
+              frequency = n_distinct(!!invoiceNumber), monetary=sum(!!invoiceAmount))
+}
 
-# drop rows with NA's
-df_retailData %>% drop_na() -> df_retailData_noNA
+# function to make bins of metrics
+getRFM_quintiles <- function(dataframe_metrics){
+  df_binnedMetrics <- data.frame(customerId = dataframe_metrics$customerId,
+             R = cut2(dataframe_metrics$recency, g = 5), 
+             F = cut2(dataframe_metrics$frequency, g = 5), 
+             M = cut2(dataframe_metrics$monetary, g = 5))
+  # reorder factor for R to be in descending order
+  df_binnedMetrics$R <- ordered(df_binnedMetrics$R, levels = sort(levels(df_binnedMetrics$R), 
+                                                                  decreasing = TRUE))
+  return(df_binnedMetrics)
+}
 
-## start of action. Function should start here
-# get metrics, monetary by summing
-df_retailData_noNA %>% 
-  group_by(CustomerID) %>%
-  summarise(recency = as.integer(Sys.Date() - max(InvoiceDate)), 
-            frequency = n_distinct(InvoiceNo), monetary = sum(total)) -> df_RFM_metrics
-df_RFM_metrics$logMonetary <- log(df_RFM_metrics$monetary)
-
-# make segments by quintiles
-df_RFM_segments <- data.frame(customerId = df_RFM_metrics$CustomerID, 
-                             R = cut2(df_RFM_metrics$recency, g = 5), 
-                             F = cut2(df_RFM_metrics$frequency, g = 5), 
-                             M = cut2(df_RFM_metrics$monetary, g = 5))
-
-# reorder factor for R to be in descending order
-df_RFM_segments$R <- ordered(df_RFM_segments$R, levels = sort(levels(df_RFM_segments$R), 
-                                                            decreasing = TRUE))
 # present scores 5, 5, 5
 df_RFM_scores <- data.frame(customerId = df_RFM_segments$customerId,
                             R_score = as.numeric(df_RFM_grading$R), 
                             F_score = as.numeric(df_RFM_grading$F), 
                             M_score = as.numeric(df_RFM_grading$M))
 
-# TODO: create 11 segments as in https://www.putler.com/rfm-analysis/
+# create 11 segments as in https://www.putler.com/rfm-analysis/
 df_RFM_scores$segment <- sapply(seq_len(nrow(df_RFM_scores)), function(i){
   ifelse((df_RFM_scores$R_score[i] >= 4 & (df_RFM_scores$F_score[i] + df_RFM_scores$M_score[i])/2 >= 4),
          "Champion", 
@@ -64,3 +63,8 @@ df_RFM_scores$segment <- sapply(seq_len(nrow(df_RFM_scores)), function(i){
                                                                            (df_RFM_scores$F_score[i] + df_RFM_scores$M_score[i])/2 <= 2),
                                                                         "Hibernating", "Lost"))))))))))
 })
+
+# Also possible to segment according to this: https://www.blastam.com/blog/rfm-analysis-boosts-sales
+# 
+# Some valuable insights on what to do further available here as well: https://www.r-bloggers.com/rfm-customer-analysis-with-r-language/
+# 
