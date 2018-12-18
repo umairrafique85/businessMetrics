4895ca6045e61b31320ac1868acbbd7491de4090
Umair Rafique <umairrafique@marketlytics.com>
2018-12-18 15:12:20 +0500

    testing RMD hosting

--- rfmR.R
+++ rfmR.R
@@ -2,6 +2,8 @@
 ## TODO: give choice to calculate monetary value by summing or by averaging  
 library(Hmisc)
 
+## RFM functions ####
+
 # function to get metrics, monetary by sum
 getRFM_metrics <- function(customerId, invoiceDate, dateBaseline = Sys.Date(), 
                            invoiceNumber, invoiceAmount, dataframe){
@@ -35,7 +37,6 @@ getRFM_scores <- function(dataframe_binnedMetrics){
              F_score = as.numeric(dataframe_binnedMetrics$F),
              M_score = as.numeric(dataframe_binnedMetrics$M))
 }
-
 
 # create 11 segments as in https://www.putler.com/rfm-analysis/
 df_RFM_scores$segment <- sapply(seq_len(nrow(df_RFM_scores)), function(i){
@@ -71,4 +72,66 @@ df_RFM_scores$segment <- sapply(seq_len(nrow(df_RFM_scores)), function(i){
 # Also possible to segment according to this: https://www.blastam.com/blog/rfm-analysis-boosts-sales
 # 
 # Some valuable insights on what to do further available here as well: https://www.r-bloggers.com/rfm-customer-analysis-with-r-language/
-# 
+
+## RF functions for bstock ####
+
+# output RF scores for data curled to storage filtered with desired event by 
+# desired website
+# wizard to spit out final scores
+rf_wizard <- function(filepath = file.choose()){
+  df_rawFiltered <- stream_in(file(filepath), flatten = TRUE)
+  df_rawFiltered$properties.time <- as.POSIXct(df_rawFiltered$properties.time,
+                                               origin = "1970-01-01 00:00.00 UTC", 
+                                               tz = "UTC")
+  df_rawFiltered %>% group_by(email = properties.email) %>% 
+    summarise(recency = as.integer(Sys.Date() - max(as.Date(properties.time))),
+              frequency = n()) -> df_RFM_metrics
+  df_binnedMetrics <- data.frame(email = df_RFM_metrics$email, 
+                                 R = cut2(df_RFM_metrics$recency, g = 5), 
+                                 F = cut2(df_RFM_metrics$frequency, g = 5))
+  df_binnedMetrics$R <- ordered(df_binnedMetrics$R, levels = sort(levels(df_binnedMetrics$R),
+                                                                  decreasing = TRUE))
+  df_RFMscores <- data.frame(email = df_binnedMetrics$email, 
+                             R_score = as.numeric(df_binnedMetrics$R),
+                             F_score = as.numeric(df_binnedMetrics$F))
+  df_RFMscores %>% arrange(desc(R_score), desc(F_score))
+}
+
+# get metrics from file ####
+rf_metrics <- function(filepath = file.choose()){
+  df_rawFiltered <- stream_in(file(filepath), flatten = TRUE)
+  df_rawFiltered$properties.time <- as.POSIXct(df_rawFiltered$properties.time,
+                                               origin = "1970-01-01 00:00.00 UTC", 
+                                               tz = "UTC")
+  df_rawFiltered %>% group_by(email = properties.email) %>% 
+    summarise(recency = as.integer(Sys.Date() - max(as.Date(properties.time))),
+              frequency = n())
+}
+
+# bin metrics from metrics dataframe
+rf_binMetrics <- function(metrics_dataframe){
+  metrics_dataframe %>% 
+    mutate(recencyBin = cut2(metrics_dataframe$recency, g = 5), 
+           frequencyBin = cut2(metrics_dataframe$frequency, g = 5)) -> df_binnedMetrics
+  df_binnedMetrics$recencyBin <- ordered(df_binnedMetrics$recencyBin, 
+                                         levels = sort(levels(df_binnedMetrics$recencyBin),
+                                                       decreasing = TRUE))
+  df_binnedMetrics %>% select(email, recency, recencyBin, frequency, frequencyBin)
+}
+
+# get scores from binned metrics dataframe
+rf_getScores <- function(binnedMetrics_dataframe){
+  data.frame(email = binnedMetrics_dataframe$email,
+             R_score = as.numeric(binnedMetrics_dataframe$recencyBin),
+             F_score = as.numeric(binnedMetrics_dataframe$frequencyBin)) %>% 
+    arrange(desc(R_score), desc(F_score))
+}
+
+# get raw from file
+getDf_events <- function(filepath = file.choose()){
+  df_rawDated <- stream_in(file(filepath), flatten = TRUE)
+  df_rawDated$properties.time <- as.Date(as.POSIXct(df_rawDated$properties.time,
+                                                    origin = "1970-01-01 00:00.00 UTC", 
+                                                    tz = "UTC"))
+  return(df_rawDated)
+}
